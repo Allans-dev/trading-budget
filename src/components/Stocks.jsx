37b7be2339383749c8a42b04d56a7efa45a0380c
Allan <allanswebdev@gmail.com>
2021-01-12 15:27:59 +1100

    income tax calculated

--- src/components/Stocks.jsx
+++ src/components/Stocks.jsx
@@ -6,74 +6,178 @@ const Stocks = () => {
   const [sellPrice, setSellPrice] = useState(0);
   const [numberOfShares, setNumberOfShares] = useState(0);
   const [showTotal, setShowTotal] = useState(false);
+  const [yearCheck, setYearCheck] = useState(false);
+  // const [taxBracket, setTaxBracket] = useState(0);
+  const [annualIncome, setAnnualIncome] = useState(0);
+
+  let taxableIncome;
+  let taxOwed;
+  let netProfit;
+  let taxBracket;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowTotal(true);
   };
 
+  const oneYearCheck = () => {
+    yearCheck ? setYearCheck(false) : setYearCheck(true);
+  };
+
+  taxableIncome = yearCheck
+    ? ((sellPrice - buyPrice) * numberOfShares) / 2 +
+      Math.round(Number(annualIncome))
+    : (sellPrice - buyPrice) * Number(numberOfShares) +
+      Math.round(Number(annualIncome));
+
+  if (taxableIncome <= 18200) {
+    taxBracket = 0;
+  }
+  if (taxableIncome > 18201 && taxableIncome <= 45000) {
+    taxBracket = 0.19;
+  }
+  if (taxableIncome > 45001 && taxableIncome <= 120000) {
+    taxBracket = 0.325;
+  }
+  if (taxableIncome > 120001 && taxableIncome <= 180000) {
+    taxBracket = 0.37;
+  }
+  if (taxableIncome > 180001) {
+    taxBracket = 0.45;
+  }
+
+  switch (taxBracket) {
+    case 0:
+      taxOwed = 0;
+      netProfit = taxableIncome;
+      break;
+    case 0.19:
+      taxOwed = (taxableIncome - 18201) * taxBracket;
+      netProfit = taxableIncome - taxOwed;
+      break;
+    case 0.325:
+      taxOwed = 5092 + (taxableIncome - 45000) * taxBracket;
+      netProfit = taxableIncome - taxOwed;
+      break;
+    case 0.37:
+      taxOwed = 29467 + (taxableIncome - 120000) * taxBracket;
+      netProfit = taxableIncome - taxOwed;
+      break;
+    case 0.45:
+      taxOwed = 51667 + (taxableIncome - 180000) * taxBracket;
+      netProfit = taxableIncome - taxOwed;
+      break;
+    default:
+      break;
+  }
+
   return (
     <article style={styles.article}>
-      <form style={styles.stocksForm} onSubmit={handleSubmit}>
-        <label style={styles.label}>
-          <span>Share Name:</span>
-          <input
-            type="text"
-            value={shareName}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label style={styles.label}>
-          <span>Unit Price (Bought):</span>
-          <input
-            type="number"
-            value={buyPrice}
-            onChange={(e) => setBuyPrice(e.target.value)}
-            required
-          />
-        </label>
-        <label style={styles.label}>
-          Unit Price (Sold):
-          <input
-            type="number"
-            value={sellPrice}
-            onChange={(e) => setSellPrice(e.target.value)}
-            required
-          />
-        </label>
-        <label style={styles.label}>
-          Number of Shares:
-          <input
-            type="number"
-            value={numberOfShares}
-            onChange={(e) => setNumberOfShares(e.target.value)}
-            required
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <section style={styles.formSection}>
+        <form style={styles.form} onSubmit={handleSubmit}>
+          <label style={styles.label}>
+            Share Name:
+            <input
+              type="text"
+              value={shareName}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label style={styles.label}>
+            Unit Price (Bought):
+            <input
+              type="number"
+              value={buyPrice}
+              onChange={(e) => setBuyPrice(e.target.value)}
+              required
+            />
+          </label>
+          <label style={styles.label}>
+            Unit Price (Sold):
+            <input
+              type="number"
+              value={sellPrice}
+              onChange={(e) => setSellPrice(e.target.value)}
+              required
+            />
+          </label>
+          <label style={styles.label}>
+            Number of Shares:
+            <input
+              type="number"
+              value={numberOfShares}
+              onChange={(e) => setNumberOfShares(e.target.value)}
+              required
+            />
+          </label>
+          <label style={styles.label}>
+            Annual Income:
+            <input
+              type="number"
+              value={annualIncome}
+              onChange={(e) => setAnnualIncome(e.target.value)}
+              required
+            />
+          </label>
+          <label style={styles.left}>
+            Held more than 1 year?{" "}
+            <input type="checkbox" onClick={oneYearCheck} />
+          </label>
 
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
       {showTotal ? (
-        <span>Profit = {(sellPrice - buyPrice) * numberOfShares} </span>
+        <div style={styles.profit}>
+          <div>
+            Taxable Income ={" "}
+            {taxableIncome > 0
+              ? Math.round((taxableIncome + Number.EPSILON) * 100) / 100
+              : 0}
+          </div>
+          <div>
+            Income tax Owed ={" "}
+            {taxOwed > 0
+              ? Math.round((taxOwed + Number.EPSILON) * 100) / 100
+              : 0}
+          </div>
+          <div>
+            Profit = {Math.round((netProfit + Number.EPSILON) * 100) / 100}
+          </div>
+          <aside style={styles.aside}>
+            The above rates do not include the Medicare levy of 2% or any low
+            income offsets.
+          </aside>
+        </div>
       ) : null}
     </article>
   );
 };
 
 const styles = {
-  article: {
+  article: { textAlign: "center" },
+  formSection: {
     display: "flex",
     justifyContent: "center",
   },
-  stocksForm: {
+  form: {
     backgroundColor: "bisque",
-    display: "flex",
+    display: "inline-flex",
     flexDirection: "column",
     flexWrap: "wrap",
+  },
+  left: {
+    textAlign: "left",
   },
   label: {
     display: "flex",
     justifyContent: "space-between",
+  },
+  profit: {
+    display: "block",
+  },
+  aside: {
+    margin: "10px",
   },
 };
 
