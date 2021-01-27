472bdedeab51b71ce6d10ad38b4b9291f4f54963
Allan <allanswebdev@gmail.com>
2021-01-27 12:56:52 +1100

    fixed deployment issue

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: "popup",
-    signInSuccessUrl: "tradingbudget.web.app",
+    signInSuccessUrl: "<>",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
