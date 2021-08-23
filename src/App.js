fc89918e08a1b8008df4a441d73287175fb2c7c2
Allan <allanswebdev@gmail.com>
2021-08-23 13:29:45 +1000

    changed landing page for google sign in

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const App = () => {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: "popup",
-    signInSuccessUrl: "/stocks",
+    signInSuccessUrl: "/",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
