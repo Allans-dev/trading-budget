fe3d99e721ee07543b220be46a180325bcd88d0f
Allan <allanswebdev@gmail.com>
2023-05-08 12:07:21 +1000

    adding tbody

--- src/components/Budget/BudgetView.jsx
+++ src/components/Budget/BudgetView.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 
-import { store } from "./budget-store";
+import { store } from './budget-store';
 
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
 
-import "./budget_style.css";
+import './budget_style.css';
 
-import "firebase/firestore";
+import 'firebase/firestore';
 
 const BudgetView = (props) => {
   const context = useContext(store);
@@ -54,81 +54,81 @@ const BudgetView = (props) => {
   // };
 
   return (
-    <article className="budget-page">
+    <article className='budget-page'>
       <form>
-        <section className="add-expense">
-          <label className="category">
-            Category:{" "}
+        <section className='add-expense'>
+          <label className='category'>
+            Category:{' '}
             <select
               required
-              id="category"
-              className="category"
+              id='category'
+              className='category'
               value={category}
               onChange={(e) => {
                 context.dispatch({
-                  type: "updateCategory",
+                  type: 'updateCategory',
                   payload: e.target.value,
                 });
               }}
             >
-              <option value="Restaurant">Restaurant</option>
-              <option value="Groceries">Groceries</option>
-              <option value="Entertainment">Entertainment</option>
-              <option value="Shopping">Shopping</option>
-              <option value="Rent/Mortgage">Rent/Mortgage</option>
-              <option value="Hobby">Hobby</option>
-              <option value="Household">Household</option>
-              <option value="Transport">Transport</option>
-              <option value="Education">Education</option>
-              <option value="Health">Health</option>
-              <option value="Other">Other</option>
+              <option value='Restaurant'>Restaurant</option>
+              <option value='Groceries'>Groceries</option>
+              <option value='Entertainment'>Entertainment</option>
+              <option value='Shopping'>Shopping</option>
+              <option value='Rent/Mortgage'>Rent/Mortgage</option>
+              <option value='Hobby'>Hobby</option>
+              <option value='Household'>Household</option>
+              <option value='Transport'>Transport</option>
+              <option value='Education'>Education</option>
+              <option value='Health'>Health</option>
+              <option value='Other'>Other</option>
             </select>
-            {category !== "Other" ? null : (
+            {category !== 'Other' ? null : (
               <input
                 required
-                className="other"
-                type="text"
+                className='other'
+                type='text'
                 value={otherCategory}
                 onChange={(e) => {
                   context.dispatch({
-                    type: "updateOtherCategory",
+                    type: 'updateOtherCategory',
                     payload: e.target.value,
                   });
                 }}
-                maxLength="11"
+                maxLength='11'
               />
             )}
           </label>
 
-          <label className="description">
+          <label className='description'>
             Description:
             <input
-              type="text"
+              type='text'
               value={description}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateDescription",
+                  type: 'updateDescription',
                   payload: e.target.value,
                 })
               }
-              maxLength="11"
+              maxLength='11'
             />
           </label>
-          <label className="cost">
-            Cost:{" "}
+          <label className='cost'>
+            Cost:{' '}
             <input
               required
-              type="number"
+              type='number'
               value={cost}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateCost",
+                  type: 'updateCost',
                   payload: Number(e.target.value),
                 })
               }
             />
           </label>
-          <button className="add-button" onClick={addExpenses}>
+          <button className='add-button' onClick={addExpenses}>
             +
           </button>
         </section>
@@ -157,95 +157,97 @@ const BudgetView = (props) => {
             <option value="year">year</option>
           </select>
         </label> */}
-        <section className="savings">
-          <label className="savings-label">
-            <span style={{ display: "block" }}>Savings Rate:</span>
+        <section className='savings'>
+          <label className='savings-label'>
+            <span style={{ display: 'block' }}>Savings Rate:</span>
             <input
-              id="savings-rate"
-              type="range"
+              id='savings-rate'
+              type='range'
               min={0}
               max={100}
               value={savingsRate}
               onChange={(e) => {
                 context.dispatch({
-                  type: "updateSavingsRate",
+                  type: 'updateSavingsRate',
                   payload: e.target.value,
                 });
                 context.dispatch({
-                  type: "updateDisplayResults",
+                  type: 'updateDisplayResults',
                   payload: false,
                 });
               }}
             ></input>
-            <div className="savings-rate">{savingsRate}%</div>
+            <div className='savings-rate'>{savingsRate}%</div>
           </label>
-          <button className="green button" onClick={calcBudget}>
-            {displayResults ? "Show Expenses" : "Save and Calculate"}
+          <button className='green button' onClick={calcBudget}>
+            {displayResults ? 'Show Expenses' : 'Save and Calculate'}
           </button>
         </section>
       </form>
 
       <section
-        className={expenseArray && expenseArray.length > 0 ? "results" : "none"}
+        className={expenseArray && expenseArray.length > 0 ? 'results' : 'none'}
       >
         {displayResults === true && netProfit > 0 ? (
-          <div className="summary">
+          <div className='summary'>
             <span>Take-Home Income: </span>
             <span>
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
               }).format(Math.round(profitBE * 100) / 100)}
             </span>
             <span>Total Expenses:</span>
             <span>
-              {" "}
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
+              {' '}
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
               }).format(Math.round(totalExpenses * 100) / 100)}
             </span>
             <span>Total Savings: </span>
             <span>
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
               }).format(Math.round(totalSavings * 100) / 100)}
             </span>
 
             {/* <span>{timeFrame} </span> */}
             <span>Net Profit: </span>
             <span>
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
               }).format(Math.round(netProfit * 100) / 100)}
             </span>
             {/* {timeFrame} */}
           </div>
         ) : displayResults === true && totalExpenses > 0 ? (
-          <div className="expenses">
+          <div className='expenses'>
             <div>
-              Total Expenses:{" "}
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
+              Total Expenses:{' '}
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
               }).format(Math.round(totalExpenses * 100) / 100)}
             </div>
           </div>
         ) : Array.isArray(expenseArray) ? (
-          <table className="expense-table">
-            {expenseArray.map((item, index) => (
-              <ExpenseItem
-                key={index}
-                index={index}
-                category={item.category}
-                description={item.description}
-                cost={item.cost}
-                deleteListItem={deleteListItem}
-                otherCategory={otherCategory}
-              />
-            ))}
+          <table className='expense-table'>
+            <tbody>
+              {expenseArray.map((item, index) => (
+                <ExpenseItem
+                  key={index}
+                  index={index}
+                  category={item.category}
+                  description={item.description}
+                  cost={item.cost}
+                  deleteListItem={deleteListItem}
+                  otherCategory={otherCategory}
+                />
+              ))}
+            </tbody>
           </table>
         ) : null}
       </section>
--- src/components/Stocks/StocksView.jsx
+++ src/components/Stocks/StocksView.jsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 
-import StocksListItem from "./StocksListItem";
+import StocksListItem from './StocksListItem';
 
 // import firebase from "firebase/app";
 // import "firebase/firestore";
 
-import { store } from "./stocks-store";
+import { store } from './stocks-store';
 
-import "./stocks_styles.css";
+import './stocks_styles.css';
 
 const StocksView = (props) => {
   const context = useContext(store);
@@ -29,95 +29,95 @@ const StocksView = (props) => {
   } = context.state;
 
   return (
-    <article className="stocks-article">
-      <section className="stocksFormSection">
+    <article className='stocks-article'>
+      <section className='stocksFormSection'>
         <form>
           <label>
             Share Name:
             <input
-              type="text"
+              type='text'
               value={stockName}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateStockName",
+                  type: 'updateStockName',
                   payload: e.target.value,
                 })
               }
-              maxLength="6"
+              maxLength='6'
               required
             />
           </label>
           <label>
             Buy Price:
             <input
-              type="number"
+              type='number'
               value={buyPrice}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateBuyPrice",
+                  type: 'updateBuyPrice',
                   payload: e.target.value,
                 })
               }
-              maxLength="5"
+              maxLength='5'
               required
             />
           </label>
           <label>
             Sold Price:
             <input
-              type="number"
+              type='number'
               value={sellPrice}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateSellPrice",
+                  type: 'updateSellPrice',
                   payload: e.target.value,
                 })
               }
-              maxLength="5"
+              maxLength='5'
               required
             />
           </label>
           <label>
             Volume:
             <input
-              type="number"
+              type='number'
               value={volume}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateVolume",
+                  type: 'updateVolume',
                   payload: e.target.value,
                 })
               }
-              maxLength="5"
+              maxLength='5'
               required
             />
           </label>
-          <label className="left">
-            Held more than 1 year?{" "}
-            <input type="checkbox" id="yearCheckBox" onClick={oneYearCheck} />
+          <label className='left'>
+            Held more than 1 year?{' '}
+            <input type='checkbox' id='yearCheckBox' onClick={oneYearCheck} />
           </label>
 
-          <button className="add-shares" onClick={addStocks}>
+          <button className='add-shares' onClick={addStocks}>
             Add Shares
           </button>
 
           <label>
             Income:
             <input
-              type="number"
+              type='number'
               value={salary}
               onChange={(e) =>
                 context.dispatch({
-                  type: "updateSalary",
+                  type: 'updateSalary',
                   payload: Number(e.target.value),
                 })
               }
             />
           </label>
 
-          <button className="button green" onClick={calculateProfit}>
-            {" "}
-            {showTotal ? "Show Stocks" : "Save and Calculate"}
+          <button className='button green' onClick={calculateProfit}>
+            {' '}
+            {showTotal ? 'Show Stocks' : 'Save and Calculate'}
           </button>
         </form>
       </section>
@@ -125,53 +125,55 @@ const StocksView = (props) => {
       {Array.isArray(stocksList) ? (
         <section className={stocksList.length > 0 ? `result-section` : `none`}>
           {showTotal ? (
-            <section className="summary">
+            <section className='summary'>
               <span>Total Taxable Income: </span>
               <span>
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
                 }).format(Math.round(totalIncome * 100) / 100)}
               </span>
               <span>Income tax Owed: </span>
               <span>
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(Math.round(taxOwed * 100) / 100)}{" "}
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(Math.round(taxOwed * 100) / 100)}{' '}
               </span>
               <span>Take-Home Income: </span>
               <span>
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
                 }).format(Math.round(profitBE * 100) / 100)}
               </span>
             </section>
           ) : (
-            <section className="stocks-display">
+            <section className='stocks-display'>
               <table>
-                <tr>
-                  <th>Code</th>
-                  <th>Buy</th>
-                  <th>Sell</th>
-                  <th>Volume</th>
-                  <th>Profit</th>
-                  <th> &gt;year</th>
-                </tr>
-                {stocksList.map((item, index) => (
-                  <StocksListItem
-                    key={index}
-                    index={index}
-                    stockName={item.stockName}
-                    buyPrice={item.buyPrice}
-                    sellPrice={item.sellPrice}
-                    volume={item.volume}
-                    yearCheck={item.yearCheck}
-                    deleteListItem={deleteListItem}
-                    iProfit={iProfit}
-                  />
-                ))}
+                <tbody>
+                  <tr>
+                    <th>Code</th>
+                    <th>Buy</th>
+                    <th>Sell</th>
+                    <th>Volume</th>
+                    <th>Profit</th>
+                    <th> &gt;year</th>
+                  </tr>
+                  {stocksList.map((item, index) => (
+                    <StocksListItem
+                      key={index}
+                      index={index}
+                      stockName={item.stockName}
+                      buyPrice={item.buyPrice}
+                      sellPrice={item.sellPrice}
+                      volume={item.volume}
+                      yearCheck={item.yearCheck}
+                      deleteListItem={deleteListItem}
+                      iProfit={iProfit}
+                    />
+                  ))}
+                </tbody>
               </table>
             </section>
           )}
