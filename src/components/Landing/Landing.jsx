9985bd2d741adc73927956bac6f26009d7613916
Allan <allanswebdev@gmail.com>
2023-05-08 12:08:08 +1000

    added full screen landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,44 +1,38 @@
-import React from "react";
+import React from 'react';
 
-import "./landing_styles.css";
+import Stocks from '../Stocks/Stocks';
+import Budget from '../Budget/Budget';
+import Analysis from '../Analysis/Analysis';
 
-const Landing = () => (
-  // {
-  //   return window.matchMedia("(max-width: 1200px)").matches ? (
-  //     <Stocks />
-  //   ) : (
-  //     <article className="landing">
-  //       <StateProvider>
-  //         <StockStateProvider>
-  //           <BudgetStateProvider>
-  //             <Stocks />
-  //             <Budget />
-  //             <Analysis />
-  //           </BudgetStateProvider>
-  //         </StockStateProvider>
-  //       </StateProvider>
-  //     </article>
-  //   );
-  // };
-  <article>
-    <section style={{ display: "flex", justifyContent: "center" }}>
-      <div>
-        <h1 style={{ width: "100%", textAlign: "center" }}>Trading Budget</h1>
-        <p>
-          Trading Budget is my finance app that records stock data from inputs
-          and saves to firebase database.{" "}
-        </p>
-        <p>
-          Please click "save and calculate" to persist to database and calculate
-          values between tabs.{" "}
-        </p>
-        <div style={{ textAlign: "center" }}>
-          <span>
-            <strong>Hello World! and Happy Trading :)</strong>
-          </span>
+import './landing_styles.css';
+
+const Landing = () =>
+  window.matchMedia('(max-width: 1200px)').matches ? (
+    <article className='landing'>
+      <section style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <h1 style={{ width: '100%', textAlign: 'center' }}>Trading Budget</h1>
+          <p>
+            Trading Budget is my finance app that records stock data from inputs
+            and saves to firebase database.{' '}
+          </p>
+          <p>
+            Please click "save and calculate" to persist to database and
+            calculate values between tabs.{' '}
+          </p>
+          <div style={{ textAlign: 'center' }}>
+            <span>
+              <strong>Hello World! and Happy Trading :)</strong>
+            </span>
+          </div>
         </div>
-      </div>
-    </section>
-  </article>
-);
+      </section>
+    </article>
+  ) : (
+    <article className='landing'>
+      <Stocks />
+      <Budget />
+      <Analysis />
+    </article>
+  );
 export default Landing;
