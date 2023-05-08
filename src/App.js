7db94a7de2cde92e3c370babdefedf00c57a69d6
Allan <allanswebdev@gmail.com>
2023-05-08 12:06:42 +1000

    removing state provider

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, matchPath } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, matchPath } from 'react-router-dom';
 
-import Landing from "./components/Landing/Landing";
-import Stocks from "./components/Stocks/Stocks";
-import Budget from "./components/Budget/Budget";
-import Analysis from "./components/Analysis/Analysis";
+import Landing from './components/Landing/Landing';
+import Stocks from './components/Stocks/Stocks';
+import Budget from './components/Budget/Budget';
+import Analysis from './components/Analysis/Analysis';
 
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
-import RingLoader from "react-spinners/RingLoader";
+import RingLoader from 'react-spinners/RingLoader';
 
-import SignIn from "./components/SignIn/SignIn";
+import SignIn from './components/SignIn/SignIn';
 
-import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
-import Disclaimer from "./components/Disclaimer/Disclaimer";
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
+import Disclaimer from './components/Disclaimer/Disclaimer';
 
-import { StateProvider } from "./main-store";
-import { StockStateProvider } from "./components/Stocks/stocks-store";
-import { BudgetStateProvider } from "./components/Budget/budget-store";
+import { StockStateProvider } from './components/Stocks/stocks-store';
+import { BudgetStateProvider } from './components/Budget/budget-store';
 
-import "./App.css";
+import './App.css';
 
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC238mryUvNxq4lScBrGNHL9dAjjHHPs4Q",
-  authDomain: "tradingbudget.firebaseapp.com",
-  projectId: "tradingbudget",
-  storageBucket: "tradingbudget.appspot.com",
-  messagingSenderId: "480429604058",
-  appId: "1:480429604058:web:7806497078f4244bff39e3",
-  measurementId: "G-555NFL2FQE",
+  apiKey: 'AIzaSyC238mryUvNxq4lScBrGNHL9dAjjHHPs4Q',
+  authDomain: 'tradingbudget.firebaseapp.com',
+  projectId: 'tradingbudget',
+  storageBucket: 'tradingbudget.appspot.com',
+  messagingSenderId: '480429604058',
+  appId: '1:480429604058:web:7806497078f4244bff39e3',
+  measurementId: 'G-555NFL2FQE',
 };
 
 if (!firebase.apps.length) {
@@ -45,7 +44,7 @@ const App = () => {
   const [authStatus, setAuthStatus] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  const policyMatch = matchPath("/logged-out-privacy-policy", {
+  const policyMatch = matchPath('/logged-out-privacy-policy', {
     path: window.location.pathname,
   });
 
@@ -63,8 +62,8 @@ const App = () => {
       },
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: "popup",
-    signInSuccessUrl: "/",
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -84,9 +83,9 @@ const App = () => {
       // },
     ],
     // Terms of service url.
-    tosUrl: "/logged-out-disclaimer",
+    tosUrl: '/logged-out-disclaimer',
     // Privacy policy url.
-    privacyPolicyUrl: "/logged-out-privacy-policy",
+    privacyPolicyUrl: '/logged-out-privacy-policy',
   };
 
   const signInAnon = () => {
@@ -98,7 +97,7 @@ const App = () => {
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        throw new Error(errorCode + ": " + errorMessage);
+        throw new Error(errorCode + ': ' + errorMessage);
         // ...
       });
   };
@@ -112,58 +111,55 @@ const App = () => {
       setIsLoading((prev) => false);
       // }
     } else {
-      throw new Error("error");
+      throw new Error('error');
     }
   });
 
   if (isLoading) {
     return (
-      <article className="loader-background">
-        <RingLoader color={"#4e4e4e"} loading={isLoading} size={100} />
+      <article className='loader-background'>
+        <RingLoader color={'#4e4e4e'} loading={isLoading} size={100} />
       </article>
     );
   }
 
   return authStatus ? (
-    <StateProvider>
-      <article className="root">
-        <Router>
-          <Header />
+    <article className='root'>
+      <Router>
+        <Header />
 
-          <Route exact path="/privacy-policy">
-            <PrivacyPolicy />
-          </Route>
+        <Route exact path='/privacy-policy'>
+          <PrivacyPolicy />
+        </Route>
 
-          <Route exact path="/disclaimer">
-            <Disclaimer />
-          </Route>
+        <Route exact path='/disclaimer'>
+          <Disclaimer />
+        </Route>
 
-          <Route exact path="/">
-            <Landing />
-          </Route>
+        <StockStateProvider>
+          <BudgetStateProvider>
+            <Route exact path='/'>
+              <Landing />
+            </Route>
+            <Route exact path='/stocks'>
+              <Stocks />
+            </Route>
+            <Route exact path='/budget'>
+              <Budget />
+            </Route>
+            <Route exact path='/analysis'>
+              <Analysis />
+            </Route>
+          </BudgetStateProvider>
+        </StockStateProvider>
 
-          <StockStateProvider>
-            <BudgetStateProvider>
-              <Route exact path="/stocks">
-                <Stocks />
-              </Route>
-              <Route exact path="/budget">
-                <Budget />
-              </Route>
-              <Route exact path="/analysis">
-                <Analysis />
-              </Route>
-            </BudgetStateProvider>
-          </StockStateProvider>
-
-          <Footer />
-        </Router>
-      </article>
-    </StateProvider>
+        <Footer />
+      </Router>
+    </article>
   ) : (
     <Router>
-      <article className="root" id="login">
-        <Route path="/">
+      <article className='root' id='login'>
+        <Route path='/'>
           <SignIn
             policyMatch={policyMatch}
             signInAnon={signInAnon}
@@ -173,11 +169,11 @@ const App = () => {
           />
         </Route>
 
-        <Route exact path="/logged-out-privacy-policy">
+        <Route exact path='/logged-out-privacy-policy'>
           <PrivacyPolicy />
         </Route>
 
-        <Route exact path="/logged-out-disclaimer">
+        <Route exact path='/logged-out-disclaimer'>
           <Disclaimer />
         </Route>
       </article>
